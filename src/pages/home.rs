87f9d1cd66361d45d616a91e354417061f8fb5f8
AMBase <milnikovalexei@gmail.com>
2023-11-29 00:28:08 +0300

    feat: styles

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -4,11 +4,6 @@ use yew::prelude::*;
 pub fn Home() -> Html {
     html! {
         <div>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-
-
         </div>
     }
 }
