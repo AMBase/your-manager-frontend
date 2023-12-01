233975a702fc4751e81d080a5b266a38d2c28e79
AMBase <milnikovalexei@gmail.com>
2023-12-02 00:36:09 +0300

    feat: wasm-bindgen fetch

--- src/components/sign_in_form.rs
+++ src/components/sign_in_form.rs
@@ -3,6 +3,18 @@ use web_sys::{EventTarget, HtmlInputElement};
 use yew::events::SubmitEvent;
 use yew::prelude::*;
 
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::spawn_local;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SignInData {
+    pub email: String,
+}
+
 #[function_component]
 pub fn SignInForm() -> Html {
     let on_submit = Callback::from(move |e: SubmitEvent| {
@@ -10,6 +22,29 @@ pub fn SignInForm() -> Html {
         let target: EventTarget = e.target().unwrap();
 
         info!("{target:?}");
+
+        spawn_local(async {
+            let url = format!("/api/v1/auth/signin");
+            let mut opts = RequestInit::new();
+            opts.method("GET");
+            opts.mode(RequestMode::Cors);
+            let request = Request::new_with_str_and_init(&url, &opts).unwrap();
+
+            request
+                .headers()
+                .set("Accept", "application/vnd.github.v3+json");
+
+            let window = web_sys::window().unwrap();
+            let resp_value = JsFuture::from(window.fetch_with_request(&request)).await;
+
+            // // `resp_value` is a `Response` object.
+            // assert!(resp_value.is_instance_of::<Response>());
+            // let resp: Response = resp_value.dyn_into().unwrap();
+            //
+            // // Convert this other `Promise` into a rust `Future`.
+            // let json = JsFuture::from(resp.json()?).await?;
+            // info!("{json:?}");
+        });
     });
 
     html! {
