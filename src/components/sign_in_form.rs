9adfbb996e56e773f9bb716ef821f50603d1bda5
AMBase <milnikovalexei@gmail.com>
2023-12-08 01:49:48 +0300

    feat: email value from input

--- src/components/sign_in_form.rs
+++ src/components/sign_in_form.rs
@@ -4,10 +4,11 @@ use yew::events::SubmitEvent;
 use yew::prelude::*;
 
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{Request, RequestInit, RequestMode};
+use web_sys::{HtmlInputElement, Request, RequestInit, RequestMode};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SignInData {
@@ -16,45 +17,55 @@ pub struct SignInData {
 
 #[function_component]
 pub fn SignInForm() -> Html {
-    let input_email = NodeRef::default();
-    let on_submit = Callback::from(move |e: SubmitEvent| {
-        e.prevent_default();
-        let target: EventTarget = e.target().unwrap();
+    let input_email = use_node_ref();
+    let on_submit = {
+        let input_email = input_email.clone();
+        Callback::from(move |e: SubmitEvent| {
+            e.prevent_default();
+            let target: EventTarget = e.target().unwrap();
+            let email_value = input_email.cast::<HtmlInputElement>().unwrap().value();
 
-        spawn_local(async {
-            let url = format!("/api/v1/auth/signin");
-            let mut opts = RequestInit::new();
-            opts.method("POST");
-            opts.mode(RequestMode::Cors);
-            let v = r#"
-                {
-                    "email": "test@example.com"
-                }
-            "#;
-            let jsv = JsValue::from_str(v);
-            opts.body(Some(&jsv));
-            let request = Request::new_with_str_and_init(&url, &opts).unwrap();
+            spawn_local(async move {
+                let url = format!("/api/v1/auth/signin");
+                let mut opts = RequestInit::new();
+                opts.method("POST");
+                opts.mode(RequestMode::Cors);
+                let v = json!({"email": email_value}).to_string();
+                let jsv = JsValue::from_str(&v);
+                opts.body(Some(&jsv));
+                let request = Request::new_with_str_and_init(&url, &opts).unwrap();
 
-            request.headers().append("Accept", "application/json");
+                let _ = request.headers().append("Accept", "application/json");
 
-            println!("{request:?}");
+                println!("{request:?}");
 
-            let window = web_sys::window().unwrap();
-            let resp_value = JsFuture::from(window.fetch_with_request(&request)).await;
+                let window = web_sys::window().unwrap();
+                let resp_value = JsFuture::from(window.fetch_with_request(&request)).await;
 
-            // // `resp_value` is a `Response` object.
-            // assert!(resp_value.is_instance_of::<Response>());
-            // let resp: Response = resp_value.dyn_into().unwrap();
-            //
-            // // Convert this other `Promise` into a rust `Future`.
-            // let json = JsFuture::from(resp.json()?).await?;
-            // info!("{json:?}");
-        });
-    });
+                // // `resp_value` is a `Response` object.
+                // assert!(resp_value.is_instance_of::<Response>());
+                // let resp: Response = resp_value.dyn_into().unwrap();
+                //
+                // // Convert this other `Promise` into a rust `Future`.
+                // let json = JsFuture::from(resp.json()?).await?;
+                // info!("{json:?}");
+            });
+        })
+    };
+
+    let on_change_email = {
+        let input_email = input_email.clone();
+        Callback::from(move |e: InputEvent| {
+            if let Some(input) = input_email.cast::<HtmlInputElement>() {
+                let value = input.value();
+                info!("{e:?}");
+            }
+        })
+    };
 
     html! {
         <form onsubmit={on_submit}>
-            <input name="email" ref={input_email.clone()} />
+            <input name="email" ref={input_email.clone()} oninput={on_change_email} />
 
             <button>{ "Отправить" }</button>
         </form>
