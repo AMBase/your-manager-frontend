cbbe23cf62a19c18041f76703e70796807332da8
AMBase <milnikovalexei@gmail.com>
2023-12-11 21:45:18 +0300

    feat: use context

--- src/pages/sign_up.rs
+++ src/pages/sign_up.rs
@@ -1,9 +1,15 @@
 use yew::prelude::*;
+use log::debug;
 
 use crate::components::sign_up_form::SignUpForm;
+use crate::api::Api;
 
 #[function_component]
 pub fn SignUp() -> Html {
+    let api: Api = use_context::<Api>().expect("no ctx found");
+
+    debug!("{:?}", api);
+
     html! {
         <div>
             <SignUpForm></SignUpForm>
